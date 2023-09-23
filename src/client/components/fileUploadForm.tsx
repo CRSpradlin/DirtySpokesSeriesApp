2e7b760dd923f75ee03671e055815f5b2e589bb0
crspradlin <github@crspradlin.org>
2023-09-23 11:59:27 -0400

    Updated file upload form styling

--- src/client/components/fileUploadForm.tsx
+++ src/client/components/fileUploadForm.tsx
@@ -37,13 +37,13 @@ export default class FileUploadForm extends React.Component {
 	public render() {
 		return (
 			<div className="content-center">
-				<span className="text-sky-700">Add a sheet: </span>
+				<span className="text-sky-700 text-lg p-6">Upload New Series Excel Report</span>
 				<form id="uploadForm" onSubmit={this.handleSubmit}>
-					<div>
-						<input name="excelFile" type="file" />
+					<div className="m-3">
+						<input name="excelFile" type="file" className="text-gray-500 border border-gray-200 rounded-lg file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-sky-500 file:text-white hover:file:bg-sky-700"/>
 					</div>
-					<div>
-						<input type="submit" value={this.state.loading?"Uploading...":"Submit"} disabled={this.state.loading} className="w-[10rem] bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm rounded-full font-semibold text-white"/>
+					<div className="m-3">
+						<input type="submit" value={this.state.loading?"Uploading...":"Submit"} disabled={this.state.loading} className={`w-[10rem] ${this.state.loading? 'bg-sky-700' : ' bg-sky-500 hover:bg-sky-700'} px-5 py-2 text-sm rounded-full font-semibold text-white`}/>
 					</div>
 				</form>
 			</div>
